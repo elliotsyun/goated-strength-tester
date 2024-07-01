1551b2d83b7c5e374c7d2576cc958affe14febe2
dokkaebiyun <elliotyun@vt.edu>
2024-06-30 22:30:37 -0400

    final changes + docstring

--- src/strength_test.py
+++ src/strength_test.py
@@ -7,12 +7,12 @@ Functions:
 
 Examples:
     To check if a password meets the required strength criteria:
-    
+
     >>> is_strong = check_length_char("StrongPass123!")
     >>> print(is_strong)
 
     To verify that a password is not a commonly used one:
-    
+
     >>> is_unique = check_comm("password123")
     >>> print(is_unique)
 
@@ -40,7 +40,20 @@ file_path = current_path / ".." / "docs" / "common_passwords.txt"
 
 
 def check_length_char(password):
+    """
+    Checks the strength of a password based on its length and character composition.
 
+    This function verifies that the password:
+    - Has no spaces.
+    - Is at least 11 characters long.
+    - Contains at least one uppercase letter, one lowercase letter, one number, and one special character (!, @, #, $, %, ^, &, *).
+
+    Parameters:
+        password (str): The password to be checked.
+
+    Returns:
+        bool: True if the password meets all the requirements, False otherwise.
+    """
     is_valid = True
     no_space = password.strip()
 
@@ -73,7 +86,17 @@ def check_length_char(password):
 
 
 def check_comm(password):
+    """
+    Checks if the password is a commonly used password.
 
+    This function compares the provided password against a list of common passwords stored in 'common_passwords.txt'.
+
+    Parameters:
+        password (str): The password to be checked.
+
+    Returns:
+        bool: True if the password is not a commonly used password, False if it is.
+    """
     with open(file_path, "r") as file:
         for line in file:
             if password.strip() == line.strip():
